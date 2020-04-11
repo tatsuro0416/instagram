8ba59cf5d727b26fa82d195764d61ec56416d88c
Tatsuro Suzuki <tatsuro.s0416@gmail.com>
2020-04-11 10:30:17 +0900

    ModifyLoinfailurePath

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails/all'
@@ -18,5 +16,6 @@ module Instaclone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
